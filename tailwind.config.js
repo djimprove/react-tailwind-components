90591b0a80c486ec6dd568e8d6b747cc8bffc0e7
dnyaneshwar <jawanednyaneshwar1@gmail.com>
2021-08-15 14:54:33 +0530

    feat: Added fontawsome

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#3B82F6',
+        secondary:'#D1D5DB',
+        error:'#EF4444',
+        success:'#6EE7B7',
+        warning:'#FBBF24',
+        info:'#60A5FA'
+      }
+    },
   },
   variants: {
     extend: {},
